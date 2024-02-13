25cca77b66ebde3a2d8eb7234b78edc61abaab78
Curtis <cyanko@codesecure.com>
2024-02-13 21:08:05 +0000

    ignore docker cache so I can get updated codesonar files

--- node-cso-builder/build.sh
+++ node-cso-builder/build.sh
@@ -1,1 +1,1 @@
-docker build -t codesecurese/node-cso-builder:8.0p0 . && docker push codesecurese/node-cso-builder:8.0p0
+docker build --no-cache -t codesecurese/node-cso-builder:8.0p0 . && docker push codesecurese/node-cso-builder:8.0p0
